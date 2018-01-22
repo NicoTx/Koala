7c83a7ad6ae24f12cf76a0aa92e1bfb63249dc7d
nicolas <nicolas@nomail.com>
2018-01-22 11:26:01 +0700

    Second commit

--- controllers/family.js
+++ controllers/family.js
@@ -1,9 +1,9 @@
 async function getFamily(ctx) {
     const mysql = require('mysql2/promise')
-    const connection = await mysql.createConnection({host:'localhost', 
-                                                     user:'root',
-                                                     password:'root',
-                                                     database:'dbKoala'});
+    const connection = await mysql.createConnection({host:DB_HOST, 
+                                                     user:DB_USER,
+                                                     password:DB_PASS,
+                                                     database:DB_NAME});
     const [results] = await connection.execute('SELECT * FROM `koala`')
     console.log(results)
     ctx.body = results
